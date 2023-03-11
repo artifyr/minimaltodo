d1db9ba971622ab5c6171991e12fd260590dcc2f
Artifyr <nyxedout@gmail.com>
2023-03-12 03:51:24 +0530

    first commit

---
+++ pages/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.text}>Made with ❤️ by Nyx</p>
+    </footer>
+  );
+};
+
+export default Footer;
